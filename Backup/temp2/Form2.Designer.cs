bc0618eff438c380711959894a457f28f7cc2e6f
krisp <krisp@krisp.com>
2016-11-06 09:59:52 -0500

    Initial commit

---
+++ Backup/temp2/Form2.Designer.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+namespace Screen_Grab
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+   
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(114, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GRAB SCREEN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.allscreen_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "GRAB AREA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.smallscreen_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(422, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "HELP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.helpbutton_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(247, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "GRAB ALL WINDOWS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.grabALL_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(506, 49);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.ShowInTaskbar = false;
+            this.Text = "SCREEN CAPTURE";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_Closing);
+            //determine start location
+            int tx, ty;
+
+            try
+            {
+                key = Registry.CurrentUser.CreateSubKey("Software\\Screen_Grab_MultiMonitor");
+                tx = (int)key.GetValue("Startx");
+                ty = (int)key.GetValue("Starty");
+                //check if last close position is within first screen
+                if (tx < screens[0].Bounds.X || tx > (screens[0].Bounds.X + screens[0].Bounds.Width) || ty < screens[0].Bounds.Y || ty > (screens[0].Bounds.Y + screens[0].Bounds.Height))
+                { this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen; }
+                else
+                {
+                    this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+                    this.Location = new System.Drawing.Point(tx, ty);
+                }
+            }
+            catch { this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen; }
+            this.ResumeLayout(false);
+
+        }
+
+       
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Backup/temp2/Form2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using Microsoft.Win32;
+
+namespace Screen_Grab
+{
+    public partial class Form2 : Form
+    {
+        int noofscreens = 0;
+        Form1[] grabwindow;
+        Screen[] screens;
+        RegistryKey key;
+        public Form2()
+        {
+            screens = Screen.AllScreens;
+            noofscreens = screens.Length;
+            InitializeComponent();
+        }
+        
+           
+        private void smallscreen_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.Refresh();
+            System.Threading.Thread.Sleep(100);
+            noofscreens = screens.Length;
+            grabwindow = new Form1[noofscreens];
+            int i = 0;
+            foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            {
+               grabwindow[i] = new Form1(this,"", screen.Bounds.X, screen.Bounds.Y, screen.Bounds.Width, screen.Bounds.Height, i);
+               grabwindow[i].Show();
+               i++;
+            }
+         }
+        private void allscreen_Click(object sender, EventArgs e) // grab all screen
+        {
+            this.Visible = false;
+            this.Refresh();
+            System.Threading.Thread.Sleep(100);
+              
+            noofscreens = screens.Length;
+            grabwindow = new Form1[noofscreens];
+            int i = 0;
+
+            foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            {   
+                grabwindow[i] = new Form1(this, "Click to capture screen "+(i+1).ToString(), screen.Bounds.X, screen.Bounds.Y, screen.Bounds.Width, screen.Bounds.Height,i);
+                grabwindow[i].Show();
+                i++;
+            }
+        }
+        private void helpbutton_Click(object sender, EventArgs e)  //help button pressed
+        {
+            MessageBox.Show("This program is used to capture either the entire screen or part of it and save it into Clipboard. It supports multiple screens. \n\nUSAGE:\n\n FOR CAPTURING ENTIRE SCREEN:\n\n 1. Select second button and then you shall see mouse cursor suggesting to click to capture that screen.\n\n2. Paste the image into any program for usage." +
+            "\n\nFOR CAPTURING A SECTION OF THE SCREEN:\n\n1. Select button one and you shall see mouse cursor changing to tilted arrows.\n\n2. Click mouse left key and keeping it pressed, select the area of interest.\n\n3. Leave the left button and the image is saved to clipboard.\n\n FOR CAPTURING ALL SCREENS:\n\n1. Press the 3rd button and All the screens are captured into one image.\n\n\nThanks for using the utility. Send in your feedback or bugs to mittaltarsem@gmail.com"
+            , "SCREEN CAPTURE HELP !!");
+        }
+
+        public void grabwholescreen(int sc)  // called when ALL screen capture; sc is screen number
+        {
+            for (int j = 0; j < noofscreens; j++) { grabwindow[j].Close(); grabwindow[j].Dispose(); }
+            capture_class.CaptureScreentoClipboard(screens[sc].Bounds.X, screens[sc].Bounds.Y,screens[sc].Bounds.Width, screens[sc].Bounds.Height);
+            this.Visible = true;  //make your original window visible
+        }
+        public void smallscreengrab(int sc, int x, int y, int x1, int y1) // grab part of screen
+        {
+            for (int j = 0; j < noofscreens; j++) { grabwindow[j].Close(); grabwindow[j].Dispose(); }
+            int finalx, finaly, finalwidth, finalheight;
+            int X1 =Math.Min(x,x1), X2 = Math.Max(x,x1), Y1 = Math.Min(y,y1), Y2=Math.Max(y,y1);
+            finalx = X1 + screens[sc].Bounds.X;
+            finaly = Y1+ screens[sc].Bounds.Y;
+            finalwidth = X2 - X1 + 1;
+            finalheight = Y2 - Y1 + 1;
+            capture_class.CaptureScreentoClipboard(finalx, finaly, finalwidth, finalheight);
+            this.Visible = true;
+  
+        }
+        public void Form2_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                key = Registry.CurrentUser.CreateSubKey("Software\\Screen_Grab_MultiMonitor");
+                key.SetValue("Startx", this.Location.X);
+                key.SetValue("Starty", this.Location.Y);
+            }
+            catch { }
+            e.Cancel = false;
+        }
+
+        private void grabALL_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            System.Threading.Thread.Sleep(100);
+           
+            int maxwidth = 0, maxheight = 0;
+            for (int i = 0; i < noofscreens; i++) 
+            {
+                if (maxwidth < (screens[i].Bounds.X + screens[i].Bounds.Width)) maxwidth = screens[i].Bounds.X + screens[i].Bounds.Width;
+                if (maxheight < (screens[i].Bounds.Y + screens[i].Bounds.Height)) maxheight = screens[i].Bounds.Y + screens[i].Bounds.Height;
+            }
+          capture_class.CaptureScreentoClipboard(0,0, maxwidth, maxheight);
+           this.Visible = true;
+        }
+
+      
+    }
+}
